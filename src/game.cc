186b035f8fab525cc03aba86c6c294e0a84d5314
Manuel Huez <huez_m@epita.fr>
2015-12-05 01:19:55 +0100

    Add better example for MainScene to add objects

--- src/game.cc
+++ src/game.cc
@@ -1,7 +1,5 @@
 #include "engine/engine.hh"
 #include "engine/scene.hh"
-#include "engine/object.hh"
-#include "engine/color.hh"
 #include "game/scenes/main-scene.hh"
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
@@ -17,11 +15,6 @@ int main()
 
     auto s = std::make_shared<Scenes::MainScene>();
     engine.scene_set(std::static_pointer_cast<E::Scene>(s));
-
-    E::Object o(sf::CircleShape(100));
-    o.circle_shape_get().setFillColor(E::Color::Orange());
-
-    engine.scene_get().object_add("circle", o);
 
     engine.run();
 
