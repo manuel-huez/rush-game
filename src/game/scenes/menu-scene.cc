f2bfc7cc355bacb603d89861b4131a7d39bcd275
Manuel Huez <huez_m@epita.fr>
2015-12-05 04:04:06 +0100

    Add Menu

--- src/game/scenes/menu-scene.cc
+++ src/game/scenes/menu-scene.cc
@@ -12,8 +12,40 @@ namespace Scenes
         bg.rectangle_shape_get().setFillColor(E::Color::Dark());
         object_add("background", bg);
 
-        E::Object title(sf::Text("Menu", E::Font::Default()));
+        E::Object title(sf::Text("Menu", E::Font::Default(), 40));
+        title.text_get().setPosition({20, 20});
         object_add("title_text", title);
+        E::Object play(sf::Text("play", E::Font::Default(), 25));
+        play.text_get().setPosition({20, 80});
+        object_add("play_text", play);
+        E::Object quit(sf::Text("quit", E::Font::Default(), 20));
+        quit.text_get().setPosition({20, 110});
+        object_add("quit_text", quit);
+
+        cur_menu_ = CurrentMenuE::Play;
+    }
+
+    void MenuScene::handle_events(sf::RenderWindow& window, sf::Time& dt)
+    {
+        Scene::handle_events(window, dt);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            object_get("play_text").text_get().setCharacterSize(25);
+            object_get("quit_text").text_get().setCharacterSize(20);
+            cur_menu_ = CurrentMenuE::Play;
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            object_get("play_text").text_get().setCharacterSize(20);
+            object_get("quit_text").text_get().setCharacterSize(25);
+            cur_menu_ = CurrentMenuE::Quit;
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
+        {
+            if (cur_menu_ == CurrentMenuE::Quit)
+                window.close();
+        }
     }
 
     void MenuScene::update(sf::RenderWindow& window, sf::Time& dt)
