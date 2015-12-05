c3a2034d5192054929ece464bdd89e3de235b5da
Jeremy Lejoux <jeremy.lejoux@epita.fr>
2015-12-05 17:11:16 +0100

    Twek generation settings

--- src/game/objects/maze.cc
+++ src/game/objects/maze.cc
@@ -9,40 +9,43 @@
 
 namespace Objects
 {
-    Maze::Maze(E::Scene& scene, sf::RenderWindow& window)
-        : GameObject(scene, window)
+  Maze::Maze(E::Scene& scene, sf::RenderWindow& window)
+    : GameObject(scene, window)
+  {
+    RMaze::World world(50, 4, 0.4f);
+    int mazeSize = 50;
+    RMaze::Maze& ma = world.get_maze();
+    float tileSize  = window.getSize().x / mazeSize;
+    for (unsigned x = 0; x < ma.size_get(); x++)
     {
-        RMaze::World world(50, 4, 0.4f);
-        int mazeSize = 50;
-        RMaze::Maze& ma = world.get_maze();
-        float tileSize  = window.getSize().x / mazeSize;
-        for (unsigned x = 0; x < ma.size_get(); x++)
+      for (unsigned y = 0; y < ma.size_get(); y++)
+      {
+        if (ma.get(x, y) != 0)
         {
-            for (unsigned y = 0; y < ma.size_get(); y++)
-            {
-                if (ma.get(x, y) != 0)
-                {
-                    E::Object o(sf::RectangleShape({tileSize, tileSize}));
-                    switch (ma.get(x, y))
-                    {
-                        case 2:
-                        case 3:
-                            o.rectangle_shape_get().setFillColor(
-                                    E::Color::White());
-                            break;
-                        default:
-                            o.rectangle_shape_get().setFillColor(
-                                    E::Color::Grey());
-                            break;
-                    }
+          E::Object o(sf::RectangleShape({tileSize, tileSize}));
+          switch (ma.get(x, y))
+          {
+            case 1:
+              o.rectangle_shape_get().setFillColor(
+                  E::Color::Grey());
+              break;
+            case 2:
+              o.rectangle_shape_get().setFillColor(
+                  E::Color::White());
+              break;
+            default:
+              o.rectangle_shape_get().setFillColor(
+                  E::Color::Dark());
+              break;
+          }
 
-                    o.rectangle_shape_get().setPosition({x * tileSize,
-                            y * tileSize});
-                    std::string name = "0" + std::to_string(x
-                            + ma.size_get() * y);
-                    object_add(name, o);
-                }
-            }
+          o.rectangle_shape_get().setPosition({x * tileSize,
+              y * tileSize});
+          std::string name = "0" + std::to_string(x
+              + ma.size_get() * y);
+          object_add(name, o);
         }
+      }
     }
+  }
 }
