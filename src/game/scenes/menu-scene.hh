a1e68ac50216fe4f8b01b7dde48767a1cd9f89d7
Manuel Huez <xu_e@epita.fr>
2015-12-05 04:22:09 +0100

    Add menu back

--- src/game/scenes/menu-scene.hh
+++ src/game/scenes/menu-scene.hh
@@ -6,12 +6,20 @@
 namespace Scenes
 {
 
+    enum CurrentMenuE {
+        Play,
+        Quit
+    };
+
     class MenuScene : public E::Scene
     {
     public:
         MenuScene(sf::RenderWindow& window);
 
+        void handle_events(sf::RenderWindow& window, sf::Time& dt);
         void update(sf::RenderWindow& window, sf::Time& dt);
+    private:
+        CurrentMenuE cur_menu_;
     };
 
 }
