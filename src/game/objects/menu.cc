b060c2f2323de661c530dff1bd281a83a8dc753a
Cyril Chemla <chemla_c@epita.fr>
2015-12-06 10:41:54 +0100

    Update menu

--- src/game/objects/menu.cc
+++ src/game/objects/menu.cc
@@ -12,18 +12,20 @@ namespace Objects
         : GameObject(scene, window)
     {
         E::Object title(sf::Text("Sprinter Cell", E::Font::Default(), 40));
-        title.text_get().setPosition({20, 20});
-        title.text_get().setColor(E::Color::White());
+        float w = 30;
+        float h = window.getSize().y / 2 - 50;
+        title.text_get().setPosition({w, h});
+        title.text_get().setColor(E::Color::Green());
         object_add("title_text", title);
 
         E::Object play(sf::Text("play", E::Font::Default(), 25));
-        play.text_get().setPosition({20, 80});
+        play.text_get().setPosition({w, h + 50});
         play.text_get().setColor(E::Color::White());
         object_add("play_text", play);
 
         E::Object quit(sf::Text("quit", E::Font::Default(), 20));
-        quit.text_get().setPosition({20, 110});
-        quit.text_get().setColor(E::Color::White());
+        quit.text_get().setPosition({w, h + 80});
+        quit.text_get().setColor(E::Color::Red());
         object_add("quit_text", quit);
     }
 }
--- src/game/scenes/menu-scene.cc
+++ src/game/scenes/menu-scene.cc
@@ -6,54 +6,68 @@
 #include "../../engine/font.hh"
 #include "../../engine/scene.hh"
 #include "../../engine/game-object.hh"
+#include "../objects/sentinel.hh"
 #include "main-scene.hh"
 #include <SFML/Graphics.hpp>
 #include <memory>
 
 namespace Scenes
 {
-    MenuScene::MenuScene(E::Engine& engine, sf::RenderWindow& window)
-        : Scene::Scene(engine, window)
+  MenuScene::MenuScene(E::Engine& engine, sf::RenderWindow& window)
+    : Scene::Scene(engine, window)
+  {
+    auto bg = std::make_shared<Objects::Background>(*this, window);
+    gobject_add("0background", std::static_pointer_cast<E::GameObject>(bg));
+
+    auto menu = std::make_shared<Objects::Menu>(*this, window);
+    gobject_add("1menu", std::static_pointer_cast<E::GameObject>(menu));
+
+    cur_menu_ = CurrentMenuE::Play;
+    for (int i = 0; i < 10; i++)
     {
-        auto bg = std::make_shared<Objects::Background>(*this, window);
-        gobject_add("0background", std::static_pointer_cast<E::GameObject>(bg));
+      auto radar =  std::make_shared<EN::Sentinel>(*this, window,
+          (rand() % (window.getSize().x - 350) + 300) / 20,
+          (rand() % (window.getSize().y - 100) + 30) / 20,
+          1, "2Radar" + i, 20, rand() % 5 + 6);
+      gobject_add("2Radar" + i, std::static_pointer_cast<E::GameObject>(radar));
+    }
+  }
 
-        auto menu = std::make_shared<Objects::Menu>(*this, window);
-        gobject_add("1menu", std::static_pointer_cast<E::GameObject>(menu));
+  void MenuScene::handle_events(E::Engine& engine,
+      sf::RenderWindow& window, sf::Time& dt)
+  {
+    Scene::handle_events(engine, window, dt);
 
-        cur_menu_ = CurrentMenuE::Play;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+      gobject_get("1menu")
+        .object_get("play_text").text_get().setCharacterSize(25);
+      gobject_get("1menu")
+        .object_get("quit_text").text_get().setCharacterSize(20);
+      cur_menu_ = CurrentMenuE::Play;
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+      gobject_get("1menu")
+        .object_get("play_text").text_get().setCharacterSize(20);
+      gobject_get("1menu")
+        .object_get("quit_text").text_get().setCharacterSize(25);
+      cur_menu_ = CurrentMenuE::Quit;
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
+    {
+      if (cur_menu_ == CurrentMenuE::Quit)
+        window.close();
+      else
+      {
+        auto s = std::make_shared<Scenes::MainScene>(engine, window);
+        engine.scene_set(std::static_pointer_cast<E::Scene>(s));
+      }
     }
 
-    void MenuScene::handle_events(E::Engine& engine,
-            sf::RenderWindow& window, sf::Time& dt)
-    {
-        Scene::handle_events(engine, window, dt);
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            gobject_get("1menu")
-                .object_get("play_text").text_get().setCharacterSize(25);
-            gobject_get("1menu")
-                .object_get("quit_text").text_get().setCharacterSize(20);
-            cur_menu_ = CurrentMenuE::Play;
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        {
-            gobject_get("1menu")
-                .object_get("play_text").text_get().setCharacterSize(20);
-            gobject_get("1menu")
-                .object_get("quit_text").text_get().setCharacterSize(25);
-            cur_menu_ = CurrentMenuE::Quit;
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
-        {
-            if (cur_menu_ == CurrentMenuE::Quit)
-                window.close();
-            else
-            {
-                auto s = std::make_shared<Scenes::MainScene>(engine, window);
-                engine.scene_set(std::static_pointer_cast<E::Scene>(s));
-            }
-        }
-    }
+
+
+
+  }
 }
